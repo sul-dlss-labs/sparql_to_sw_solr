14585e5d965a4ecc34ee4adced970afd82bd87d2
Naomi Dushay <ndushay@stanford.edu>
2017-04-10 19:04:49 -0700

    imprint_display field populated

--- lib/sparql_to_sw_solr/instance_pub_fields.rb
+++ lib/sparql_to_sw_solr/instance_pub_fields.rb
@@ -8,8 +8,8 @@ module SparqlToSwSolr
 
       def add_publication_fields(doc)
         add_pub_year_fields(doc)
-        doc[:imprint_display] = ''
-        doc[:pub_search] = ''
+        doc[:imprint_display] = imprint_display
+        # doc[:pub_search] = ''
         doc
       end
 
@@ -23,9 +23,19 @@ module SparqlToSwSolr
         # year for display
         doc[:pub_year_ss] = @year_display_str # new way (used for mods)
         doc[:pub_date] = @year_display_str # old way, for display string ??  replaced by specific ones?
-        # for specificity in display ...
+        # for specificity in SW display ... if this were a marc or mods record ...
         # doc[:creation_year_isi]
         doc[:publication_year_isi] = @year_int
+      end
+
+      def init_year_fields
+        solution_values_for_binding(pub_year_solutions, :pub_year).each do |value|
+          value = value[0, 4]
+          @year_int ||= Stanford::Mods::DateParsing.year_int_from_date_str(value)
+          @year_display_str ||= Stanford::Mods::DateParsing.date_str_for_display(value)
+          @sort_str ||= Stanford::Mods::DateParsing.sortable_year_string_from_date_str(value)
+          break if @year_int && @year_display_str && @sort_str
+        end
       end
 
       def pub_year_solutions
@@ -41,15 +51,64 @@ module SparqlToSwSolr
         sparql.query(query)
       end
 
-      def init_year_fields
-        solution_values_for_binding(pub_year_solutions, :pub_year).each do |value|
-          value = value[0, 4]
-          @year_int ||= Stanford::Mods::DateParsing.year_int_from_date_str(value)
-          @year_display_str ||= Stanford::Mods::DateParsing.date_str_for_display(value)
-          @sort_str ||= Stanford::Mods::DateParsing.sortable_year_string_from_date_str(value)
-          break if @year_int && @year_display_str && @sort_str
-        end
+      def imprint_display
+        edition = solution_values_for_binding(edition_solns, :edition).first
+        pub_info = solution_values_for_binding(publication_solns, :pub_info).first
+        concatenate_values(edition, ' - ', concatenate_values(pub_info, '. ', manu_info))
       end
+
+      def manu_info
+        soln = manufacture_solns.first
+        manu_place = soln.manu_place.to_s if soln && soln.bindings.keys.include?(:manu_place)
+        manu_agent = soln.manu_agent.to_s if soln && soln.bindings.keys.include?(:manu_agent)
+        manu_date = soln.manu_date.to_s if soln && soln.bindings.keys.include?(:manu_date)
+        concatenate_values(manu_place, ' : ', concatenate_values(manu_agent, ', ', manu_date))
+      end
+
+      def edition_solns
+        query = "#{BF_NS_DECL}
+          SELECT ?edition
+          WHERE {
+            <#{instance_uri}> bf:editionStatement ?edition
+          }"
+        sparql.query(query)
+      end
+
+      def publication_solns
+        query = "#{BF_NS_DECL}
+          SELECT ?pub_info
+          WHERE {
+            <#{instance_uri}> bf:provisionActivityStatement ?pub_info .
+          }"
+        sparql.query(query)
+      end
+
+      def manufacture_solns
+        # the query below avoids Manufacture info from marc 008 by filter to blank nodes for place
+        #   and filtering out edtf datatype for dates
+        query = "#{BF_NS_DECL}
+          SELECT ?manu_place ?manu_agent ?manu_date
+          WHERE {
+            <#{instance_uri}> bf:provisionActivity ?provision .
+            ?provision a bf:Manufacture .
+
+            OPTIONAL {
+              ?provision bf:place ?place .
+              FILTER isBlank (?place) .
+              ?place rdfs:label ?manu_place .
+            }
+            OPTIONAL {
+              ?provision bf:agent ?agent .
+              ?agent rdfs:label ?manu_agent .
+            }
+            OPTIONAL {
+              ?provision bf:date ?manu_date .
+              FILTER (datatype(?manu_date) != <http://id.loc.gov/datatypes/edtf> )
+            }
+          }"
+        sparql.query(query)
+      end
+
     end
   end
 end
--- spec/instance_pub_fields_spec.rb
+++ spec/instance_pub_fields_spec.rb
@@ -11,35 +11,39 @@ RSpec.describe SparqlToSwSolr::InstanceSolrDoc::InstancePubFields do
     allow(sparql_conn).to receive(:query).and_return(solutions)
   end
 
-  shared_examples 'single-valued field' do |solr_field_name|
-    let(:values_array) { ['2000', '1090'] }
-    let(:solr_field) { doc_hash[solr_field_name] }
-
-    it 'is a String (first value if there are multiple values)' do
-      values_array.each do |value|
-        solutions << RDF::Query::Solution.new(pub_year: value)
-      end
-      expect(solr_field).not_to be_an Array
-      if solr_field.is_a?(Integer)
-        expect(solr_field).to eq values_array.first.to_i
-      else
-        expect(solr_field).to eq values_array.first
-      end
-    end
-
-    it 'nil if there is no value' do
-      expect(solr_field).to eq nil
-    end
-  end
-
   context '#add_publication_fields' do
     it 'includes pub year fields' do
       expect(isd).to receive(:add_pub_year_fields)
       isd.solr_doc_hash
     end
+    it 'includes imprint_display field' do
+      expect(isd).to receive(:imprint_display)
+      isd.solr_doc_hash
+    end
   end
 
   context '#add_pub_year_fields' do
+    shared_examples 'single-valued field' do |solr_field_name|
+      let(:values_array) { ['2000', '1090'] }
+      let(:solr_field) { doc_hash[solr_field_name] }
+
+      it 'is a String (first value if there are multiple values)' do
+        values_array.each do |value|
+          solutions << RDF::Query::Solution.new(pub_year: value)
+        end
+        expect(solr_field).not_to be_an Array
+        if solr_field.is_a?(Integer)
+          expect(solr_field).to eq values_array.first.to_i
+        else
+          expect(solr_field).to eq values_array.first
+        end
+      end
+
+      it 'nil if there is no value' do
+        expect(solr_field).to eq nil
+      end
+    end
+
     shared_examples 'copes with wacko values' do |solr_field_name|
       # note that these are all values provided by https://github.com/lcnetdev/marc2bibframe2 from our data ...
       ['2014-09', '2015/9999', '2016-1987-87', '2013-09XX-XX'].each do |raw_value|
@@ -90,4 +94,128 @@ RSpec.describe SparqlToSwSolr::InstanceSolrDoc::InstancePubFields do
     end
   end
 
+  context '#imprint_display' do
+    let(:edition_val) { 'first edition' }
+    let(:pub_info) { 'place : agent, year.' }
+    let(:manu_place) { 'manu_place' }
+    let(:manu_agent) { 'manu_agent' }
+    let(:manu_date) { '1990' }
+
+    it 'gets edition info from bf:editionStatement' do
+      expect(sparql_conn).to receive(:query).with(a_string_matching('editionStatement')).and_return(solutions)
+      isd.send(:edition_solns)
+    end
+    it 'gets publication information from bf:provisionActivityStatement' do
+      expect(sparql_conn).to receive(:query).with(a_string_matching('provisionActivityStatement')).and_return(solutions)
+      isd.send(:publication_solns)
+    end
+    it 'gets manufacture information from bf:provisionActivity of type bf:Manufacture' do
+      expect(sparql_conn).to receive(:query).with(a_string_matching('provisionActivity ')).and_return(solutions)
+      isd.send(:manufacture_solns)
+    end
+    it 'concatenates edition, publication and manufacture information' do
+      ed_solns = RDF::Query::Solutions.new
+      ed_solns << RDF::Query::Solution.new(edition: edition_val)
+      expect(isd).to receive(:edition_solns).and_return(ed_solns)
+      pub_solns = RDF::Query::Solutions.new
+      pub_solns << RDF::Query::Solution.new(pub_info: pub_info)
+      expect(isd).to receive(:publication_solns).and_return(pub_solns)
+      manu_solns = RDF::Query::Solutions.new
+      manu_solns << RDF::Query::Solution.new(manu_place: manu_place, manu_agent: manu_agent, manu_date: manu_date)
+      expect(isd).to receive(:manufacture_solns).and_return(manu_solns)
+      exp_val = "#{edition_val} - #{pub_info}. #{manu_place} : #{manu_agent}, #{manu_date}"
+      expect(isd.send(:imprint_display)).to eq exp_val
+    end
+    it 'is nil if there is no value for edition, publication or manufacture' do
+      expect(isd.send(:imprint_display)).to be nil
+    end
+    it 'publication + manufacture if no edition info' do
+      pub_solns = RDF::Query::Solutions.new
+      pub_solns << RDF::Query::Solution.new(pub_info: pub_info)
+      expect(isd).to receive(:publication_solns).and_return(pub_solns)
+      manu_solns = RDF::Query::Solutions.new
+      manu_solns << RDF::Query::Solution.new(manu_place: manu_place, manu_agent: manu_agent, manu_date: manu_date)
+      expect(isd).to receive(:manufacture_solns).and_return(manu_solns)
+      expect(isd.send(:imprint_display)).to eq "#{pub_info}. #{manu_place} : #{manu_agent}, #{manu_date}"
+    end
+    it 'edition + manufacture if no publication info' do
+      ed_solns = RDF::Query::Solutions.new
+      ed_solns << RDF::Query::Solution.new(edition: edition_val)
+      expect(isd).to receive(:edition_solns).and_return(ed_solns)
+      manu_solns = RDF::Query::Solutions.new
+      manu_solns << RDF::Query::Solution.new(manu_place: manu_place, manu_agent: manu_agent, manu_date: manu_date)
+      expect(isd).to receive(:manufacture_solns).and_return(manu_solns)
+      expect(isd.send(:imprint_display)).to eq "#{edition_val} - #{manu_place} : #{manu_agent}, #{manu_date}"
+    end
+    it 'edition + publication if no manufacture info' do
+      ed_solns = RDF::Query::Solutions.new
+      ed_solns << RDF::Query::Solution.new(edition: edition_val)
+      expect(isd).to receive(:edition_solns).and_return(ed_solns)
+      pub_solns = RDF::Query::Solutions.new
+      pub_solns << RDF::Query::Solution.new(pub_info: pub_info)
+      expect(isd).to receive(:publication_solns).and_return(pub_solns)
+      expect(isd.send(:imprint_display)).to eq "#{edition_val} - #{pub_info}"
+    end
+    it 'edition info if no publication or manufacture info' do
+      solutions << RDF::Query::Solution.new(edition: edition_val)
+      expect(isd.send(:imprint_display)).to eq edition_val
+    end
+    it 'publication info if no edition or manufacture info' do
+      solutions << RDF::Query::Solution.new(pub_info: pub_info)
+      expect(isd.send(:imprint_display)).to eq pub_info
+    end
+    it 'manufacture info if no edition or publication info' do
+      solutions << RDF::Query::Solution.new(manu_place: manu_place, manu_agent: manu_agent, manu_date: manu_date)
+      expect(isd.send(:imprint_display)).to eq "#{manu_place} : #{manu_agent}, #{manu_date}"
+    end
+    it 'uses first editionStatement if more than one exists' do
+      solutions << RDF::Query::Solution.new(edition: edition_val)
+      solutions << RDF::Query::Solution.new(edition: 'foo')
+      expect(isd.send(:imprint_display)).to eq edition_val
+    end
+    it 'uses first provisionActivityStatement if more than one exists' do
+      solutions << RDF::Query::Solution.new(pub_info: pub_info)
+      solutions << RDF::Query::Solution.new(pub_info: 'foo')
+      expect(isd.send(:imprint_display)).to eq pub_info
+    end
+  end
+
+  context '#manu_info' do
+    let(:manu_place) { 'manu_place' }
+    let(:manu_agent) { 'manu_agent' }
+    let(:manu_date) { '1990' }
+    it 'manufacture info is a concatenation of place, agent and date' do
+      solutions << RDF::Query::Solution.new(manu_place: manu_place, manu_agent: manu_agent, manu_date: manu_date)
+      expect(isd.send(:manu_info)).to eq "#{manu_place} : #{manu_agent}, #{manu_date}"
+    end
+    it 'manufacture info can be missing manu_place' do
+      solutions << RDF::Query::Solution.new(manu_agent: manu_agent, manu_date: manu_date)
+      expect(isd.send(:manu_info)).to eq "#{manu_agent}, #{manu_date}"
+    end
+    it 'manufacture info can be missing manu_agent' do
+      solutions << RDF::Query::Solution.new(manu_place: manu_place, manu_date: manu_date)
+      expect(isd.send(:manu_info)).to eq "#{manu_place} : #{manu_date}"
+    end
+    it 'manufacture info can be missing manu_date' do
+      solutions << RDF::Query::Solution.new(manu_place: manu_place, manu_agent: manu_agent)
+      expect(isd.send(:manu_info)).to eq "#{manu_place} : #{manu_agent}"
+    end
+    it 'manufacture info can be only manu_place' do
+      solutions << RDF::Query::Solution.new(manu_place: manu_place)
+      expect(isd.send(:manu_info)).to eq manu_place
+    end
+    it 'manufacture info can be only manu_agent' do
+      solutions << RDF::Query::Solution.new(manu_agent: manu_agent)
+      expect(isd.send(:manu_info)).to eq manu_agent
+    end
+    it 'manufacture info can be only manu_date' do
+      solutions << RDF::Query::Solution.new(manu_date: manu_date)
+      expect(isd.send(:manu_info)).to eq manu_date
+    end
+    it 'uses first bf:Manufacture provisionActivity if more than one exists' do
+      solutions << RDF::Query::Solution.new(manu_place: manu_place, manu_date: manu_date)
+      solutions << RDF::Query::Solution.new(manu_agent: manu_agent)
+      expect(isd.send(:manu_info)).to eq "#{manu_place} : #{manu_date}"
+    end
+  end
 end
